ffbc7726e94666822ada05e4c22610fdbb5787c2
Bill.Dagou <billdagou@gmail.com>
2020-03-04 16:03:32 +0800

    Add FlashMessagesViewHelper

---
+++ Classes/ViewHelpers/FlashMessagesViewHelper.php
@@ -0,0 +1,45 @@
+<?php
+namespace Dagou\Bootstrap\ViewHelpers;
+
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+class FlashMessagesViewHelper extends AbstractViewHelper {
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = FALSE;
+    /**
+     * @var array
+     */
+    protected $classes = [
+        FlashMessage::INFO  => 'primary',
+        FlashMessage::OK => 'success',
+        FlashMessage::ERROR => 'danger',
+    ];
+
+    public function initializeArguments() {
+        $this->registerArgument('identifier', 'string', 'Flash-message queue identifier');
+        $this->registerArgument('severity', 'string', 'Optional severity, must be either of one of \TYPO3\CMS\Core\Messaging\FlashMessage constants');
+        $this->registerArgument('flush', 'boolean', 'Flush the message queue or not', FALSE, TRUE);
+    }
+
+    /**
+     * @return string
+     */
+    public function render() {
+        $severity = isset($this->classes[$this->arguments['severity']]) ? $this->classes[$this->arguments['severity']] : NULL;
+        $getAllMessagesFunc = $this->arguments['flush'] ? 'getAllMessagesAndFlush' : 'getAllMessages';
+
+        $content = '';
+
+        if (count($flashMessages = $this->controllerContext->getFlashMessageQueue($this->arguments['identifier'])->$getAllMessagesFunc($severity))) {
+            /** @var FlashMessage $flashMessage */
+            foreach ($flashMessages as $flashMessage) {
+                $content .= '<div class="alert alert-'.(self::$classes[$flashMessage->getSeverity()] ?? 'primary').'">'.$flashMessage->getMessage().'</div>';
+            }
+        }
+
+        return $content;
+    }
+}
